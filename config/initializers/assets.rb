5e96da621bd1d2323816e117189e79de451c069a
Oops <oops@debian.Sainfuw>
2018-10-24 10:36:41 -0300

    add fix to images routes and add dashboad index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor','assets', 'fonts', 'feather')
-Rails.application.config.assets.paths << Rails.root.join('vendor','assets', 'images', 'faces', 'female')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
