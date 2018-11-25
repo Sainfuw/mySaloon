5ffc12e569af7c0531c1037fd0d74874d2c07982
Sainfuw <sainfuw@aol.com>
2018-11-25 07:34:49 -0300

    dependent destroy for booking services on booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', primary_key: :id, foreign_key: :author_id
   belongs_to :customer
-  has_many :booking_services
+  has_many :booking_services, dependent: :destroy
   has_many :services, through: :booking_services
   has_many :billings, through: :booking_services
 
