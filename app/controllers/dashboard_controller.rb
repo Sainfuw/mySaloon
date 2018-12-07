9ccdfb061a70956f55287567a76f97e01d3714d6
Sainfuw <sainfuw@aol.com>
2018-12-07 10:56:28 -0300

    only admin joins to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,22 @@
 class DashboardController < ApplicationController
 
   def index
-    @services_count = Service.actives.count
-    @bookings_count = Booking.count
-    @bookings_completed_count = Booking.where(status: 'completed').count
-    @book_comp_prom = (@bookings_completed_count * 100) / @bookings_count
-    @clients_count = Customer.count
+    if current_user.admin?
+      @services_count = Service.actives.count
+      @bookings_count = Booking.count
+      @bookings_completed_count = Booking.where(status: 'completed').count
+      @book_comp_prom = (@bookings_completed_count * 100) / @bookings_count
+      @clients_count = Customer.count
 
-    @services_prom = Service.eager_load(:bookings).map { |s| { id: s.id, percent: (s.bookings.size*100)/@bookings_count } }
-    @top_services = @services_prom.group_by { |s| s[:percent] }.sort_by { |k, v| -k }.map(&:last).flatten.first(4)
+      @services_prom = Service.eager_load(:bookings).map { |s| { id: s.id, percent: (s.bookings.size*100)/@bookings_count } }
+      @top_services = @services_prom.group_by { |s| s[:percent] }.sort_by { |k, v| -k }.map(&:last).flatten.first(4)
 
-    @bookings = Booking.last(5).reverse
+      @bookings = Booking.last(5).reverse
+    else
+      redirect_to bookings_url
+    end
   end
+
 
   def get_bookings_per_day
     @date = Date.today - 3
